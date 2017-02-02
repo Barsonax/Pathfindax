1b35524ddec9058e749216202528fac2fe83d5b0
Barsonax <rick-van-dam@outlook.com>
2017-02-02 21:12:30 +0100

    develop: more portable libraries

--- Source/Code/Pathfindax.Duality/Properties/AssemblyInfo.cs
+++ Source/Code/Pathfindax.Duality/Properties/AssemblyInfo.cs
@@ -13,14 +13,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
-// für COM-Komponenten. Wenn Sie auf einen Typ in dieser Assembly von 
-// COM zugreifen müssen, legen Sie das ComVisible-Attribut für diesen Typ auf "true" fest.
-[assembly: ComVisible(false)]
-
-// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
-[assembly: Guid("00c8792c-39b8-4558-acf9-03013402301a")]
-
 // Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
 //
 //      Hauptversion
