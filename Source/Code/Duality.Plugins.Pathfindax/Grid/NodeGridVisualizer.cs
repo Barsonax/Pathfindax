96f2133c5fa5cfecfeaace914d907e9ace5c8786
Rick van Dam <rick@blacksnow.nl>
2017-02-09 13:07:34 +0100

    PAX-8: comments, cleanup

--- Source/Code/Duality.Plugins.Pathfindax/Grid/NodeGridVisualizer.cs
+++ Source/Code/Duality.Plugins.Pathfindax/Grid/NodeGridVisualizer.cs
@@ -4,55 +4,58 @@ using Pathfindax.Nodes;
 
 namespace Duality.Plugins.Pathfindax.Grid
 {
-	/// <summary>
-	/// Class for visualizing a <see cref="INodeGrid{TNode}"/>
-	/// </summary>
-	public class NodeGridVisualizer
-	{
+    /// <summary>
+    /// Class for visualizing a <see cref="INodeGrid{TNode}"/>
+    /// </summary>
+    public class NodeGridVisualizer
+    {
+        /// <summary>
+        /// The collision category that will be used for visualizing. If the collision category in a connection would collide with this collision category it will not be drawn.
+        /// </summary>
 		public PathfindaxCollisionCategory CollisionCategory { get; set; }
-		private readonly INodeGrid<IGridNode> _nodeNetwork;
-		private readonly float _nodeSize;
-		private Vector2 _offset;
+        private readonly INodeGrid<IGridNode> _nodeNetwork;
+        private readonly float _nodeSize;
+        private Vector2 _offset;
 
-		public NodeGridVisualizer(INodeGrid<IGridNode> nodeNetwork)
-		{
-			_nodeNetwork = nodeNetwork;
-			_nodeSize = nodeNetwork.NodeSize.X * 0.3f;
-			_offset = new Vector2(nodeNetwork.Offset.X, nodeNetwork.Offset.Y);
-		}
+        public NodeGridVisualizer(INodeGrid<IGridNode> nodeNetwork)
+        {
+            _nodeNetwork = nodeNetwork;
+            _nodeSize = nodeNetwork.NodeSize.X * 0.3f;
+            _offset = new Vector2(nodeNetwork.Offset.X, nodeNetwork.Offset.Y);
+        }
 
-		/// <summary>
-		/// This will draw the <see cref="IGridNode"/> and their connectons to other <see cref="IGridNode"/> in the <see cref="INodeGrid{TNode}"/>
-		/// </summary>
-		public void Draw(IDrawDevice device)
-		{
-			if (_nodeNetwork != null)
-			{
-				var canvas = new Canvas(device, new CanvasBuffer());
-				canvas.State.ZOffset = -5;
-				foreach (var node in _nodeNetwork)
-				{
-					canvas.State.ColorTint = ColorRgba.LightGrey;
-					if (node.Walkable)
-					{
-						canvas.DrawCircle(node.Position.X + _offset.X, node.Position.Y + _offset.Y, _nodeSize);
-					}
-					else
-					{
-						canvas.FillCircle(node.Position.X + _offset.X, node.Position.Y + _offset.Y, _nodeSize);
-					}
-					canvas.State.ColorTint = new ColorRgba(199, 21, 133);
-					foreach (var connection in node.Connections)
-					{
-						if (connection == null || (connection.CollisionCategory & CollisionCategory) != 0)
-						{
-							continue;
-						}
-						var vector = (connection.To.Position - node.Position) * 0.5f;
-						canvas.DrawDashLine(node.Position.X + _offset.X, node.Position.Y + _offset.Y, node.Position.X + vector.X + _offset.X, node.Position.Y + vector.Y + _offset.Y);
-					}
-				}
-			}
-		}
-	}
+        /// <summary>
+        /// This will draw the <see cref="IGridNode"/> and their connections to other <see cref="IGridNode"/> in the <see cref="INodeGrid{TNode}"/>
+        /// </summary>
+        public void Draw(IDrawDevice device)
+        {
+            if (_nodeNetwork != null)
+            {
+                var canvas = new Canvas(device, new CanvasBuffer());
+                canvas.State.ZOffset = -5;
+                foreach (var node in _nodeNetwork)
+                {
+                    canvas.State.ColorTint = ColorRgba.LightGrey;
+                    if (node.Walkable)
+                    {
+                        canvas.DrawCircle(node.WorldPosition.X, node.WorldPosition.Y, _nodeSize);
+                    }
+                    else
+                    {
+                        canvas.FillCircle(node.WorldPosition.X, node.WorldPosition.Y, _nodeSize);
+                    }
+                    canvas.State.ColorTint = new ColorRgba(199, 21, 133);
+                    foreach (var connection in node.Connections)
+                    {
+                        if (connection == null || (connection.CollisionCategory & CollisionCategory) != 0)
+                        {
+                            continue;
+                        }
+                        var vector = (connection.To.Position - node.Position) * 0.5f;
+                        canvas.DrawDashLine(node.WorldPosition.X, node.WorldPosition.Y, node.WorldPosition.X + vector.X + _offset.X, node.WorldPosition.Y + vector.Y);
+                    }
+                }
+            }
+        }
+    }
 }
--- Source/Code/Pathfindax/PathfindEngine/PathRequestProcesser.cs
+++ Source/Code/Pathfindax/PathfindEngine/PathRequestProcesser.cs
@@ -45,7 +45,7 @@ namespace Pathfindax.PathfindEngine
 					path = postProcess.Process(path);
 				}
 			}
-			return new CompletedPath(path.Select(x => x.Position + nodeGrid.Offset).ToArray(), pathRequest.Callback);
+			return new CompletedPath(path.Select(x => x.WorldPosition).ToArray(), pathRequest.Callback);
 		}
 	}
 }
