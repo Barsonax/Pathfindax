6baf5f414370acacc324e6b4116408a69d416012
Barsonax <rick-van-dam@outlook.com>
2017-10-24 22:31:40 +0200

    develop: comments, changed test to use a non obsolete method.

--- Source/Code/Duality.Plugins.Pathfindax/Components/Interfaces/IPathProvider.cs
+++ Source/Code/Duality.Plugins.Pathfindax/Components/Interfaces/IPathProvider.cs
@@ -1,7 +1,13 @@
 ﻿namespace Duality.Plugins.Pathfindax.Components
 {
+	/// <summary>
+	/// Interface for providing a path. For instance so that it can be visualized with the <see cref="PathVisualizer"/>
+	/// </summary>
 	public interface IPathProvider
 	{
+		/// <summary>
+		/// The path.
+		/// </summary>
 		Vector2[] Path { get; }
 	}
 }
--- Source/Code/Duality.Plugins.Pathfindax/PathfindEngine/NonGridPathfinderProxy.cs
+++ Source/Code/Duality.Plugins.Pathfindax/PathfindEngine/NonGridPathfinderProxy.cs
@@ -5,27 +5,61 @@ using Pathfindax.Primitives;
 
 namespace Duality.Plugins.Pathfindax.PathfindEngine
 {
-    public class NonGridPathfinderProxy : PathfinderProxy<ISourceNode, ISourceNodeNetwork<ISourceNode>>
+	/// <summary>
+	/// Proxy for pathfinding on a <see cref="ISourceNodeNetwork{ISourceNode}"/>
+	/// </summary>
+	public class NonGridPathfinderProxy : PathfinderProxy<ISourceNode, ISourceNodeNetwork<ISourceNode>>
     {
-        public PathRequest RequestPath(Vector3 start, Vector3 end, byte agentSize = 1, PathfindaxCollisionCategory collisionLayer = PathfindaxCollisionCategory.None)
+		/// <summary>
+		/// Requests a new path
+		/// </summary>
+		/// <param name="start"></param>
+		/// <param name="end"></param>
+		/// <param name="agentSize"></param>
+		/// <param name="collisionLayer"></param>
+		/// <returns></returns>
+		public PathRequest RequestPath(Vector3 start, Vector3 end, byte agentSize = 1, PathfindaxCollisionCategory collisionLayer = PathfindaxCollisionCategory.None)
         {
             return RequestPath(start.X, start.Y, end.X, end.Y, agentSize, collisionLayer);
         }
 
-        public PathRequest RequestPath(Vector2 start, Vector2 end, byte agentSize = 1, PathfindaxCollisionCategory collisionLayer = PathfindaxCollisionCategory.None)
+		/// <summary>
+		/// Requests a new path
+		/// </summary>
+		/// <param name="start"></param>
+		/// <param name="end"></param>
+		/// <param name="agentSize"></param>
+		/// <param name="collisionLayer"></param>
+		/// <returns></returns>
+		public PathRequest RequestPath(Vector2 start, Vector2 end, byte agentSize = 1, PathfindaxCollisionCategory collisionLayer = PathfindaxCollisionCategory.None)
         {
             return RequestPath(start.X, start.Y, end.X, end.Y, agentSize, collisionLayer);
         }
 
-        public PathRequest RequestPath(PositionF start, PositionF end, byte agentSize = 1, PathfindaxCollisionCategory collisionLayer = PathfindaxCollisionCategory.None)
+		/// <summary>
+		/// Requests a new path
+		/// </summary>
+		/// <param name="start"></param>
+		/// <param name="end"></param>
+		/// <param name="agentSize"></param>
+		/// <param name="collisionLayer"></param>
+		/// <returns></returns>
+		public PathRequest RequestPath(PositionF start, PositionF end, byte agentSize = 1, PathfindaxCollisionCategory collisionLayer = PathfindaxCollisionCategory.None)
         {
             return RequestPath(start.X, start.Y, end.X, end.Y, agentSize, collisionLayer);
         }
 
-        /// <summary>
-        /// Requests a new path
-        /// </summary>
-        public PathRequest RequestPath(float x1, float y1, float x2, float y2, byte agentSize = 1, PathfindaxCollisionCategory collisionLayer = PathfindaxCollisionCategory.None)
+		/// <summary>
+		/// Requests a new path
+		/// </summary>
+		/// <param name="x1"></param>
+		/// <param name="y1"></param>
+		/// <param name="x2"></param>
+		/// <param name="y2"></param>
+		/// <param name="agentSize"></param>
+		/// <param name="collisionLayer"></param>
+		/// <returns></returns>
+		public PathRequest RequestPath(float x1, float y1, float x2, float y2, byte agentSize = 1, PathfindaxCollisionCategory collisionLayer = PathfindaxCollisionCategory.None)
         {
             var startNode = Pathfinder.SourceNodeNetwork.GetNode(x1, y1);
             var endNode = Pathfinder.SourceNodeNetwork.GetNode(x2, y2);
