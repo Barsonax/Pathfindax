6c2c5d5ba60d751efebfd241ba2151f1c0e6523c
Barsonax <rick-van-dam@outlook.com>
2017-11-27 21:23:44 +0100

    #40 array2d unit test split up

--- Source/Code/Pathfindax.Test/Tests/Array2DTests.cs
+++ Source/Code/Pathfindax.Test/Tests/Array2DTests.cs
@@ -10,19 +10,27 @@ namespace Pathfindax.Test.Tests
 	class Array2DTests
 	{
 		[Test, TestCaseSource(typeof(Array2DTests), nameof(Array2DTestCases))]
-		public void Array2D_2DimensionalAccess(Array2D<int> array2D, bool shouldCrash, Point2 pointToCheck)
+		public void Array2D_2DimensionalAccess_CorrectValue(Array2D<int> array2D, Point2 pointToCheck)
 		{
-			if (shouldCrash)
+			var value = array2D[pointToCheck.X, pointToCheck.Y];
+			Assert.AreEqual(value, array2D.Array[value]);
+		}
+
+		[Test, TestCaseSource(typeof(Array2DTests), nameof(Array2DOutOfBoundsTestCases))]
+		public void Array2D_2DimensionalAccess_OutOfBounds(Array2D<int> array2D, Point2 pointToCheck)
+		{
+			Assert.Catch<IndexOutOfRangeException>(() =>
 			{
-				Assert.Catch<IndexOutOfRangeException>(() =>
-				{
-					var unused = array2D[pointToCheck.X, pointToCheck.Y];
-				});
-			}
-			else
+				var unused = array2D[pointToCheck.X, pointToCheck.Y];
+			});
+		}
+
+		public static IEnumerable Array2DOutOfBoundsTestCases
+		{
+			get
 			{
-				var value = array2D[pointToCheck.X, pointToCheck.Y];
-				Assert.AreEqual(value, array2D.Array[value]);
+				yield return GenerateArray2DTestCase(3, 5, new Point2(1, 5));
+				yield return GenerateArray2DTestCase(3, 5, new Point2(3, 2));
 			}
 		}
 
@@ -30,21 +38,19 @@ namespace Pathfindax.Test.Tests
 		{
 			get
 			{
-				yield return GenerateArray2DTestCase(3, 5, false, new Point2(1, 3));
-				yield return GenerateArray2DTestCase(3, 5, true, new Point2(1, 5));
-				yield return GenerateArray2DTestCase(3, 5, true, new Point2(3, 2));
-				yield return GenerateArray2DTestCase(9, 5, false, new Point2(3, 2));
+				yield return GenerateArray2DTestCase(3, 5, new Point2(1, 3));
+				yield return GenerateArray2DTestCase(9, 5, new Point2(3, 2));
 			}
 		}
 
-		private static TestCaseData GenerateArray2DTestCase(int width, int height, bool shouldCrash, Point2 pointToCheck)
+		private static TestCaseData GenerateArray2DTestCase(int width, int height, Point2 pointToCheck)
 		{
 			var array = new int[width * height];
 			for (int i = 0; i < array.Length; i++)
 			{
 				array[i] = i;
 			}
-			return new TestCaseData(new Array2D<int>(array, width, height), shouldCrash, pointToCheck).SetName($"Size: {width}:{height}, ShouldCrash: {shouldCrash} Point: {pointToCheck}");
+			return new TestCaseData(new Array2D<int>(array, width, height), pointToCheck).SetName($"Size: {width}:{height}, Point: {pointToCheck}");
 		}
 	}
 }
