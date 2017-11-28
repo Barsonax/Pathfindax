93f96092ced09a8bfe692a5cb766152dd52797bc
Barsonax <rick-van-dam@outlook.com>
2017-11-28 17:54:45 +0100

    develop: moved transformer to separate file

--- Source/Code/Pathfindax/Graph/GridTransformer.cs
+++ Source/Code/Pathfindax/Graph/GridTransformer.cs
@@ -3,30 +3,6 @@ using Pathfindax.Utils;
 
 namespace Pathfindax.Graph
 {
-	public class Transformer
-	{
-		public Vector2 Position { get; }
-		public Vector2 Scale { get; }
-
-		public Transformer(Vector2 scale, Vector2 position = default(Vector2))
-		{
-			Position = position;
-			Scale = scale;
-		}
-
-		public Vector2 ToLocal(float worldX, float worldY)
-		{
-			var x = (worldX - Position.X) / Scale.X;
-			var y = (worldY - Position.Y) / Scale.Y;
-			return new Vector2(x, y);
-		}
-
-		public Vector2 ToWorld(float localX, float localY)
-		{
-			return new Vector2(localX * Scale.X + Position.X, localY * Scale.Y + Position.Y);
-		}
-	}
-
 	public class GridTransformer : Transformer
 	{
 		public Point2 GridSize { get; }
---
+++ Source/Code/Pathfindax/Graph/Transformer.cs
@@ -0,0 +1,28 @@
+﻿using Duality;
+
+namespace Pathfindax.Graph
+{
+	public class Transformer
+	{
+		public Vector2 Position { get; }
+		public Vector2 Scale { get; }
+
+		public Transformer(Vector2 scale, Vector2 position = default(Vector2))
+		{
+			Position = position;
+			Scale = scale;
+		}
+
+		public Vector2 ToLocal(float worldX, float worldY)
+		{
+			var x = (worldX - Position.X) / Scale.X;
+			var y = (worldY - Position.Y) / Scale.Y;
+			return new Vector2(x, y);
+		}
+
+		public Vector2 ToWorld(float localX, float localY)
+		{
+			return new Vector2(localX * Scale.X + Position.X, localY * Scale.Y + Position.Y);
+		}
+	}
+}
