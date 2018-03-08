e670f05f391305048db13a84165192f9172255c2
Rick van Dam <Rick.van.Dam@avivasolutions.nl>
2018-03-08 12:13:14 +0100

    added single source omparer unit tests

---
+++ Source/Code/Pathfindax.Test/Tests/PathfindEngine/PathfindEngineTestCases.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Duality;
+using NSubstitute;
+using NUnit.Framework;
+using Pathfindax.Factories;
+using Pathfindax.Graph;
+using Pathfindax.Nodes;
+using Pathfindax.PathfindEngine;
+
+namespace Pathfindax.Test.Tests.PathfindEngine
+{
+	public class PathfindEngineTestCases
+	{
+		public static IEnumerable SingleSourcePathRequestComparerTestCases
+		{
+			get
+			{
+				yield return new TestCaseData(Substitute.For<IPathRequest>(), Substitute.For<IPathRequest>(), true);
+
+				yield return new TestCaseData(((Func<IPathRequest>)(() =>
+					{
+						var request = Substitute.For<IPathRequest>();
+						request.CollisionCategory.Returns(PathfindaxCollisionCategory.Cat1);
+						return request;
+					})).Invoke(), ((Func<IPathRequest>)(() =>
+					{
+						var request = Substitute.For<IPathRequest>();
+						return request;
+					})).Invoke(), false);
+			}
+		}
+	}
+}
---
+++ Source/Code/Pathfindax.Test/Tests/PathfindEngine/SingleSourcePathRequestComparerTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using Pathfindax.PathfindEngine;
+
+namespace Pathfindax.Test.Tests.PathfindEngine
+{
+	[TestFixture]
+	public class SingleSourcePathRequestComparerTests
+	{
+		[Test, TestCaseSource(typeof(PathfindEngineTestCases), nameof(PathfindEngineTestCases.SingleSourcePathRequestComparerTestCases))]
+		public void Compare(IPathRequest request1, IPathRequest request2, bool expected)
+		{
+			var comparer = new SingleSourcePathRequestComparer();
+			Assert.AreEqual(expected, comparer.Equals(request1, request2));
+		}
+	}
+}
