eb263ab0d08b9f3aabba2d59bd35802716c9ce1f
Rick van Dam <Rick.van.Dam@avivasolutions.nl>
2018-04-13 11:51:43 +0200

    fixed Pathfindax.Duality by rewriting the dualityunittest attribute to work with xunit

--- Source/Code/Pathfindax.Duality.Test/NodeGridRayCasterTest.cs
+++ Source/Code/Pathfindax.Duality.Test/NodeGridRayCasterTest.cs
@@ -1,35 +1,36 @@
-﻿//using System.Collections.Generic;
-//using Duality;
-//using Duality.Components;
-//using Duality.Components.Physics;
-//using Duality.Resources;
-//using Xunit;
+﻿using System.Collections.Generic;
+using Duality;
+using Duality.Components;
+using Duality.Components.Physics;
+using Duality.Resources;
+using Xunit;
 
-//namespace Pathfindax.Duality.Test
-//{
-//	public class NodeGridRayCasterTest
-//	{
-//		public Scene SetupScene()
-//		{
-//			var scene = new Scene();
-//			Scene.SwitchTo(new ContentRef<Scene>(scene));
-//			return scene;
-//		}
+namespace Pathfindax.Duality.Test
+{
+    [Collection(nameof(DualityCollection))]
+    public class NodeGridRayCasterTest
+    {
+        public Scene SetupScene()
+        {
+            var scene = new Scene();
+            Scene.SwitchTo(new ContentRef<Scene>(scene));
+            return scene;
+        }
 
-//		[Fact]
-//		public void RayCast_DualityEnvironment_Passes()
-//		{
-//			var scene = SetupScene();
-//			var obj = new GameObject("Object");
-//			obj.AddComponent<Transform>();
-//			var body = obj.AddComponent<RigidBody>();
-//			body.BodyType = BodyType.Static; //Rigidbody has to be a static one if we want to use chain and polygon shapes
-//			body.AddShape(new ChainShapeInfo(new List<Vector2> { new Vector2(0, 0), new Vector2(1f, 0) }));
-//			scene.AddObject(obj);
-//			var hits = new RawList<RayCastData>();
-//			RigidBody.RayCast(new Vector2(0.5f, 1f), new Vector2(0.5f, -1f), hitData => hitData.Fraction, hits);
-//			Assert.Single(hits);
-//		}
-//	}
-//}
+        [Fact]
+        public void RayCast_DualityEnvironment_Passes()
+        {
+            var scene = SetupScene();
+            var obj = new GameObject("Object");
+            obj.AddComponent<Transform>();
+            var body = obj.AddComponent<RigidBody>();
+            body.BodyType = BodyType.Static; //Rigidbody has to be a static one if we want to use chain and polygon shapes
+            body.AddShape(new ChainShapeInfo(new List<Vector2> { new Vector2(0, 0), new Vector2(1f, 0) }));
+            scene.AddObject(obj);
+            var hits = new RawList<RayCastData>();
+            RigidBody.RayCast(new Vector2(0.5f, 1f), new Vector2(0.5f, -1f), hitData => hitData.Fraction, hits);
+            Assert.Single(hits);
+        }
+    }
+}
 
--- Source/Code/Pathfindax.Duality.Test/SourceNodeGridFactoryTests.cs
+++ Source/Code/Pathfindax.Duality.Test/SourceNodeGridFactoryTests.cs
@@ -1,34 +1,35 @@
-﻿//using Duality;
-//using Pathfindax.Collections;
-//using Pathfindax.Factories;
-//using Pathfindax.Graph;
-//using Pathfindax.Nodes;
-//using Xunit;
+﻿using Duality;
+using Pathfindax.Collections;
+using Pathfindax.Factories;
+using Pathfindax.Graph;
+using Pathfindax.Nodes;
+using Xunit;
 
-//namespace Pathfindax.Duality.Test
-//{
-//	public class SourceNodeGridFactoryTests
-//	{
-//		[Fact]
-//		public void CalculateGridNodeClearances_FilledNodeGrid_Passes()
-//		{
-//			const int width = 4;
-//			const int height = 4;
-//			var factory = new DefinitionNodeGridFactory();
-//			var nodeGrid = factory.GeneratePreFilledArray(GenerateNodeGridConnections.All, width, height);
-//			var sourceNodeGrid = new DefinitionNodeGrid(nodeGrid, new Vector2(1, 1));
-//			var astarNodeNetwork = new AstarNodeNetwork(sourceNodeGrid, new BrushfireClearanceGenerator(sourceNodeGrid, 5));
-			
-//			var sourceNodeNetworkCat1 = new Array2D<AstarNode>(astarNodeNetwork.GetCollisionLayerNetwork(PathfindaxCollisionCategory.Cat1), width, height);
-//			Assert.Equal(1, sourceNodeNetworkCat1[0, 0].Clearance);
-//			Assert.Equal(1, sourceNodeNetworkCat1[1, 0].Clearance);
-//			Assert.Equal(1, sourceNodeNetworkCat1[2, 0].Clearance);
-//			Assert.Equal(1, sourceNodeNetworkCat1[3, 0].Clearance);
+namespace Pathfindax.Duality.Test
+{
+    [Collection(nameof(DualityCollection))]
+    public class SourceNodeGridFactoryTests
+    {
+        [Fact]
+        public void CalculateGridNodeClearances_FilledNodeGrid_Passes()
+        {
+            const int width = 4;
+            const int height = 4;
+            var factory = new DefinitionNodeGridFactory();
+            var nodeGrid = factory.GeneratePreFilledArray(GenerateNodeGridConnections.All, width, height);
+            var sourceNodeGrid = new DefinitionNodeGrid(nodeGrid, new Vector2(1, 1));
+            var astarNodeNetwork = new AstarNodeNetwork(sourceNodeGrid, new BrushfireClearanceGenerator(sourceNodeGrid, 5));
 
-//			Assert.Equal(1, sourceNodeNetworkCat1[0, 1].Clearance);
-//			Assert.Equal(3, sourceNodeNetworkCat1[1, 1].Clearance);
-//			Assert.Equal(3, sourceNodeNetworkCat1[2, 1].Clearance);
-//			Assert.Equal(1, sourceNodeNetworkCat1[3, 1].Clearance);
-//		}
-//	}
-//}
+            var sourceNodeNetworkCat1 = new Array2D<AstarNode>(astarNodeNetwork.GetCollisionLayerNetwork(PathfindaxCollisionCategory.Cat1), width, height);
+            Assert.Equal(1, sourceNodeNetworkCat1[0, 0].Clearance);
+            Assert.Equal(1, sourceNodeNetworkCat1[1, 0].Clearance);
+            Assert.Equal(1, sourceNodeNetworkCat1[2, 0].Clearance);
+            Assert.Equal(1, sourceNodeNetworkCat1[3, 0].Clearance);
+
+            Assert.Equal(1, sourceNodeNetworkCat1[0, 1].Clearance);
+            Assert.Equal(3, sourceNodeNetworkCat1[1, 1].Clearance);
+            Assert.Equal(3, sourceNodeNetworkCat1[2, 1].Clearance);
+            Assert.Equal(1, sourceNodeNetworkCat1[3, 1].Clearance);
+        }
+    }
+}
